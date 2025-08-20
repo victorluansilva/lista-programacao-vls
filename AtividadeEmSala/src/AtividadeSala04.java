0f2f6a3574e7c9fa0fe858dc0c2b25814ad992d4
victorluansilva <victorluansilva@gmail.com>
2025-08-20 19:37:21 -0300

    Atividades em sala dia 20 e material de referência

---
+++ AtividadeEmSala/src/AtividadeSala04.java
@@ -0,0 +1,21 @@
+public class AtividadeSala04 {
+
+    public static void main(String[] args){
+        //Imprima o resultado do valor final da variável x
+        int x = 0;
+        x += 1;
+        System.out.println("x += 1 = " + x);
+        x++;
+        System.out.println("x ++ = " + x);
+        x-=2;
+        System.out.println("x -= 2 = " + x);
+        //Qual o valor final de x?
+        System.out.println("Valor de X atual é:" + x);
+        // Imprima o valor de x em cada operação
+        //Qual o resultado de x se
+        x--;
+        System.out.print("x-- = " + x);
+    }
+
+
+}
---
+++ Resumos/Resumo.java
@@ -0,0 +1,92 @@
+public class Resumo {
+
+    public static void main(String[] args){
+
+        // TIPOS PRIMITIVOS DE VARIÁVEIS
+
+        //Textuais
+        String texto = "Só pode receber texto";
+        char caracter = 'A';
+
+        //Numéricos
+        int numeroInteiro = 5; //tipo inteiro
+        double numeroReal = 3.14; //tipo real
+
+        //Lógico
+        boolean logico = false;
+
+        // VARIÁVEIS E CONSTANTES
+
+        String nomeDaVariavel = "";
+        int idade = 0;
+        double totalDaMedia;
+        // CONSTANTE
+        final double PI = 3.14;
+
+        // OPERADORES
+
+        //ATRIBUIÇÃO DE VALOR
+        int a = 5;
+        int b = 1;
+        a += b; // a = a + b; result: 6
+        System.out.println(a);
+        a -= b; // a = a - b; result: 5
+        System.out.println(a);
+        //OPERAÇÕES DE RELAÇÃO COMPARAÇÃO
+        boolean result = false;
+        result = (a == b);
+        System.out.println(result);
+        result = (a != b);
+        System.out.println(result);
+        result = (a < b);
+        System.out.println(result);
+        result = (a > b);
+        System.out.println(result);
+        result = (a >= b);
+        System.out.println(result);
+        result = (a <= b);
+        System.out.println(result);
+        //OPERADORES LÓGICOS
+        boolean varA = false; boolean varB = false;
+        result = (varA && varB);// OPERADOR E(AND)
+        result = (varA || varB);// OPERADOR OU(OR)
+        result = (!varA);//OPERADOR NÃO(NOT)
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
