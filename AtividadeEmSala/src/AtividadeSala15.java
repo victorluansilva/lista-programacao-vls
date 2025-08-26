7cf52191dee86410f23ffe48a28117f6289d433c
victorluansilva <victorluansilva@gmail.com>
2025-08-26 19:21:13 -0300

    Atividade 15

---
+++ AtividadeEmSala/src/AtividadeSala15.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class AtividadeSala15 {
+    
+    public static double soma(double x, double y){
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+
+        //Utilizando funções e procedimentos faça:
+        /*
+
+        1- Quatros operações básicas da matemática (soma, subtração, divisão, multiplicação)
+        2- Metodo para calcular a potência de um número real elevado a um número inteiro, tal como: potenciacao(numero,expoente);
+        3- Faça um menu para escolha das 5 operações e a opção de sair;
+        4- Crie um procedimento que dado o 'char' da operação, imprima os valores, tal como: printResultado(op,x,y);
+        6- Se opção 'sair' escolhido, listar todas as operações realizadas;
+         */
+
+    }
+}
