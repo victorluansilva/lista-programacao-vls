cc0e4fe62d0d1b5909c637a5a6bdb03b93ed1850
victorluansilva <victorluansilva@gmail.com>
2025-08-26 19:50:53 -0300

    Referência para atividade 15

--- AtividadeEmSala/src/AtividadeSala15.java
+++ AtividadeEmSala/src/AtividadeSala15.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
 
 public class AtividadeSala15 {
-    
-    public static double soma(double x, double y){
+
+    public static double soma(double x, double y) {
         return x + y;
+    }
+
+    public static void printResultado(char operador, double x, double y, double resultado){
+        System.out.println(x + " " + operador + y + " = " + resultado);
     }
 
     public static void main(String[] args) {
@@ -17,6 +21,56 @@ public class AtividadeSala15 {
         4- Crie um procedimento que dado o 'char' da operação, imprima os valores, tal como: printResultado(op,x,y);
         6- Se opção 'sair' escolhido, listar todas as operações realizadas;
          */
+        Scanner leia = new Scanner(System.in);
+        int opcao = -1;
+
+        do {
+            System.out.println("Calculadora: \n Escolha uma das opções abaixo.");
+            System.out.println(
+                    "1- Soma \n" +
+                            "2- Subtração \n" +
+                            "3- Multiplicação \n" +
+                            "4- Divisão \n" +
+                            "5- Potenciação \n" +
+                            "0- Sair \n"
+
+
+            );
+            opcao = leia.nextInt();
+            double x = 0;
+            double y = 0;
+            switch (opcao){
+                case 1:
+                    System.out.println("Soma: ");
+                    System.out.println("Digite valor para X:");
+                    x = leia.nextDouble();
+                    System.out.println("Digite valor para Y:");
+                    y = leia.nextDouble();
+                    printResultado('+',x,y,soma(x, y));
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 0:
+                    System.out.println("Você escolheu sair. \n " +
+                            "Segue abaixo o resumo das operações realizadas.");
+                    break;
+                default:
+                    System.out.println("Opção não encontrada!");
+                    break;
+
+            }
+        } while (opcao != 0);
 
     }
 }
