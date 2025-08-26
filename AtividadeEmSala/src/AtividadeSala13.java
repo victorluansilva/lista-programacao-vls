41e0e46fc9f3876cd10f1da3540601c476c14e64
victorluansilva <victorluansilva@gmail.com>
2025-08-25 21:59:24 -0300

    Atividade 14 - Métodos

--- AtividadeEmSala/src/AtividadeSala13.java
+++ AtividadeEmSala/src/AtividadeSala13.java
@@ -1,17 +1,40 @@
 import java.util.Scanner;
+
 public class AtividadeSala13 {
+
+    public static void printBoletim(String[] vetor, double[][] matriz){
+        for (int linha = 0; linha < 3; linha++) {
+            System.out.println(vetor[linha]);
+            for (int coluna = 0; coluna < 3; coluna++) {
+                System.out.println((coluna + 1) + " etapa: "
+                        + matriz[linha][coluna] + " pontos");
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Scanner leia = new Scanner(System.in);
-        //MATRIZES
-        //double[][] boletim = new double[3][3];
+//MATRIZES
+//double[][] boletim = new double[3][3];
         double[][] boletim = {
-                {0,0,0},
-                {0,0,0},
-                {0,0,0}
+                {0, 0, 0},
+                {0, 0, 0},
+                {0, 0, 0}
         };
-
-        String[] materias = {"Matemática","Português","Física"};
-
+        String[] materias = {"Matemática", "Português", "Física"};
+        for (int linha = 0; linha < 3; linha++) {
+            System.out.println("Digite as notas de " + materias[linha]);
+            for (int coluna = 0; coluna < 3; coluna++) {
+                System.out.println("Digite a nota do " + (coluna + 1) + "º trimestre");
+                boletim[linha][coluna] = leia.nextDouble();
+            }
+        }
+        System.out.println("Boletim:");
+    // Matemática:
+    // 1ª etapa: 10 pontos
+    // 2ª etapa: 10 pontos
+    // 3ª etapa: 10 pontos
+        printBoletim(materias, boletim);
 
 
     }
---
+++ AtividadeEmSala/src/AtividadeSala14.java
@@ -0,0 +1,19 @@
+public class AtividadeSala14 {
+
+    //Função
+    public static int soma(int x, int y){
+        return x + y;
+    }
+    //Procedimento
+    public static void imprimir(int valor){
+        System.out.println(valor);
+    }
+
+
+    public static void main(String[] args) {
+
+        //int valor = soma(20, 35);
+        imprimir(soma(20, 35));
+    }
+
+}
