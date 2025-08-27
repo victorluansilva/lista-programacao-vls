8668c8ecc4b43f3a97404947a1bb294fb70ee88b
victorluansilva <victorluansilva@gmail.com>
2025-08-26 22:05:05 -0300

    Exemplo de recrussão finalizado

--- AtividadeEmSala/src/AtividadeSala16.java
+++ AtividadeEmSala/src/AtividadeSala16.java
@@ -12,7 +12,11 @@ public class AtividadeSala16 {
     }
 
     public static void main(String[] args) {
-
+    Scanner leia = new Scanner(System.in);
+        System.out.println("Fatorial \n " +
+                "Digite um valor para x:");
+        int resultado = fatorial(leia.nextInt());
+        System.out.println("O fatorial do número digitado é:" + resultado);
 
     }
 }
