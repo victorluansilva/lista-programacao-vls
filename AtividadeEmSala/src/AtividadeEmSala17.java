f34f9539366c5817bade2bdd47221d0346de45f1
victorluansilva <victorluansilva@gmail.com>
2025-08-28 19:18:49 -0300

    Finalizado atividades de exemplo em sala de aula

---
+++ AtividadeEmSala/src/AtividadeEmSala17.java
@@ -0,0 +1,13 @@
+public class AtividadeEmSala17 {
+
+    public static void main(String[] args) {
+
+        String[] palavras = {"Alex", "Carlos", "Cauã", "Kauman com K", "Xamuel", "Tiago"};
+        char letra = 'A';
+        int numero = 1;
+        for (int i = palavras.length-1; i >= 0; i--) {
+            System.out.println(palavras[i]);
+        }
+    }
+
+}
--- AtividadeEmSala/src/AtividadeSala01.java
+++ AtividadeEmSala/src/AtividadeSala01.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 public class AtividadeSala01 {
     //Crie uma variável string para receber nome;
     //Crie uma variável tipo inteiro para receber idade;
@@ -5,12 +7,18 @@ public class AtividadeSala01 {
 
     public static void main(String[] args){
         //Variáveis
-        String nome = "";
-        nome = "Victor";
-        int idade = 29;
+        //Exemplo de declaração de uma variável
+        String logradouro = "Rua Açonorte, 326, Bairro Siderurgia. Ouro Branco - MG CEP 36420-000";
+        // Alteração de uma variável
+        logradouro = "Rua Rubem Alves, 311, Cidade Universitária";
+        // Leitura de uma variável
+        System.out.println("O valor na variável logradouro é:" + logradouro);
+        //Exemplo de declaração de uma contante
+        final int PESO_PRATO = 250;
+        // Leitura da constante
+        System.out.println("O valor da constate é:" + PESO_PRATO);
 
-        System.out.println("Nome: " + nome);
-        System.out.println("Idade: " + idade);
+
 
     }
 
