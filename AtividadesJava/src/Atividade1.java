9b108c0fdfb5b3640f05f394d614827787eef1ec
victorluansilva <victorluansilva@gmail.com>
2025-08-28 19:19:29 -0300

    Atividade1 - resolvida

--- AtividadesJava/src/Atividade1.java
+++ AtividadesJava/src/Atividade1.java
@@ -1,5 +1,12 @@
 public class Atividade1 {
 
+    public static void main(String[] args) {
+        String nome = "Victor";
+        String cidade = "São Gonçalo";
+        final String PAIS = "Brasil";
 
+        System.out.println("Olá, eu sou " + nome + " moro em " +
+                cidade + " /" + PAIS      );
+    }
 
 }
